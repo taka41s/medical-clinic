b6f7930e288b9ef7497148943573bb800ba8d174
taka41s <gutonunes99@gmail.com>
2024-02-23 19:02:39 -0300

    removing test migration

--- config/migrations/20240223205748_add_users_table.sql
+++ config/migrations/20240223205748_add_users_table.sql
@@ -1,17 +1,9 @@
 -- +goose Up
--- SQL in section 'Up' is executed when this migration is applied
-
-CREATE TABLE IF NOT EXISTS users (
-    id SERIAL PRIMARY KEY,
-    username VARCHAR(255) NOT NULL UNIQUE,
-    email VARCHAR(255) NOT NULL UNIQUE,
-    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
-);
-
-INSERT INTO users (username, email) VALUES ('john_doe', 'john@example.com');
-INSERT INTO users (username, email) VALUES ('jane_doe', 'jane@example.com');
+-- +goose StatementBegin
+SELECT 'up SQL query';
+-- +goose StatementEnd
 
 -- +goose Down
--- SQL in section 'Down' is executed when this migration is rolled back
-
-DROP TABLE IF EXISTS users;
+-- +goose StatementBegin
+SELECT 'down SQL query';
+-- +goose StatementEnd
